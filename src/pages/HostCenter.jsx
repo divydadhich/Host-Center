27770528f199cccd2a2f252a341fa632518e8904
Divy Dadhich <divydadhich1234@gmail.com>
2025-12-24 15:11:45 +0530

    hieee

--- src/pages/HostCenter.jsx
+++ src/pages/HostCenter.jsx
@@ -10,7 +10,7 @@ export default function HostCenter() {
       <div className="w-full max-w-[430px] min-h-screen pb-8">
 
         {/* HEADER */}
-        <PageHeader title="Agency" />
+        <PageHeader title="Admin" />
 
         {/* CONTENT WRAPPER */}
         <div className="px-4">
